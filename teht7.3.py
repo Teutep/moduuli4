7b5304d934fddafbf92cb59dfc3cf68d4089ffa5
Teuvo <teuvo.almgren@gmail.com>
2022-09-12 10:19:52 +0300

    moduuli7

--- teht7.3.py
+++ teht7.3.py
@@ -6,18 +6,19 @@ while Program_Running:
     Input = input("""Kirjoita \"uusi\" jos haluat syöttää uuden kentän
 Kirjoita \"olemassaoleva\" jos haluat etsiä olemassaolevista.
 Kirjoita \"lopeta\" jos haluat lopettaa ohjelman käytön.
->""").lower()
+> """).lower()
     if Input == "olemassaoleva":
         Airport = input("Syötä lentokentän ICAO koodi\n").upper()
-        Airport = Airport
         if Airport in Airports:
             print(f"Lentokentän {Airport} ICAO koodia vastaava lentokenttä on: {Airports[Airport]}\n")
         else:
             print(f"Lentokenttää ei löytynyt")
     elif Input == "uusi":
-        Airport = input("Syötä lentokentän ICAO koodi\n")
-        Airport = Airport.upper()
-        AirportName = input("Syötä lentokentän nimi\n")
-        Airports[Airport] = AirportName
+        Airport = input("Syötä lentokentän ICAO koodi\n").upper()
+        AirportName = input("Syötä lentokentän nimi\n").title()
+        if Airport in Airports or AirportName in Airports:
+            print("ICAO-koodi tai lentokenttä löytyy jo hakemistosta\n")
+        else:
+            Airports[Airport] = AirportName
     elif Input == "lopeta":
         Program_Running = False
